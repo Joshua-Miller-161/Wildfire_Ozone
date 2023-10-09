9c4d55fa67863127279e7d8ed868d5137eed3a34
Joshua-Miller-161 <jmill233@vols.utk.edu>
2023-10-09 16:47:29 +0100

    Added the 'DownSample' function

--- misc/misc_utils.py
+++ misc/misc_utils.py
@@ -27,3 +27,26 @@ def Funnel(start_size, end_size, r=np.e):
                 i += 1
             sizes.append(end_size)
             return sizes
+        
+
+def DownSample(data, downsample_rate, axis, delete=False):
+    '''
+    Made by ChatGPT - Extracts data points separated by skip along the given axis
+
+    Returns downsampled data.
+    
+    - data (ndarray) - the data
+    - skip (int) - the number of elements that are skiped when downsampling
+    - axis (int) - the axis on which to downsample
+    - delete (bool) - whether or not to delete the original data in order to save memory
+    '''
+    slices       = [slice(None)] * data.ndim
+    slices[axis] = slice(None, None, downsample_rate)
+    new_data     = data[tuple(slices)]
+    
+    print('         Orig. shape :', np.shape(data), "----> new shape :", np.shape(new_data))
+
+    if delete:
+        del(data)
+
+    return new_data
