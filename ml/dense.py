a98ff864475579391382a00e6c2ec2838629fc1f
Joshua-Miller-161 <jmill233@vols.utk.edu>
2024-07-05 19:22:31 +0100

    dense

--- ml/dense.py
+++ ml/dense.py
@@ -14,7 +14,7 @@ from keras.layers import Input, Concatenate, Dense, TimeDistributed, Permute, Re
 
 sys.path.append(os.getcwd())
 from ml.ml_utils import Funnel
-from ml.custom_keras_layers import Transpose
+from ml.custom_keras_layers import TransformerBlock
 #====================================================================
 def MakeDense(config_path, 
               x_data_shape=(1164, 5, 28, 14, 8), 
@@ -26,6 +26,8 @@ def MakeDense(config_path,
         config = yaml.load(c, Loader=yaml.FullLoader)
 
     assert (config['MODEL_TYPE'] == 'Dense'), "'MODEL_TYPE' must be 'Dense'. Got: "+str(config['MODEL_TYPE'])
+
+    num_trans = config['HYPERPARAMETERS']['dense_hyperparams_dict']['num_trans']
     #----------------------------------------------------------------
     input_layer = Input(shape=x_data_shape[1:])
 
@@ -33,21 +35,35 @@ def MakeDense(config_path,
     # Spatial part
     spat_flat = Reshape((input_layer.shape[1], -1))(input_layer)
     
-    num_spat_nuerons = Funnel(spat_flat.shape[-1], y_data_shape[2] * y_data_shape[3] * y_data_shape[4], r=30)
-    for i in range(np.shape(num_spat_nuerons)[0]):
+    num_spat_nuerons = Funnel(spat_flat.shape[-1], np.prod(y_data_shape[2:]), r=10)
+    for i in range(1, np.shape(num_spat_nuerons)[0]):
         spat_flat = Dense(units=num_spat_nuerons[i], activation='relu')(spat_flat)
-        spat_flat = LayerNormalization()(spat_flat)
-        spat_flat = Dropout(rate=0.1)(spat_flat)
+        if (i == 1):
+            for _ in range(num_trans):
+                spat_flat = TransformerBlock(embed_dim=spat_flat.shape[-1],
+                                             num_heads=4,
+                                             ff_dim=spat_flat.shape[-1],
+                                             attn_axes=2)(spat_flat)
+        else:
+            spat_flat = LayerNormalization()(spat_flat)
+            spat_flat = Dropout(rate=0.1)(spat_flat)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Time part
     time = Permute((2, 3, 1, 4), input_shape=input_layer.shape[1:])(input_layer)
     time_flat = Reshape((time.shape[1]*time.shape[2], time.shape[3]*time.shape[4]))(time)
     
-    num_time_nuerons = Funnel(time_flat.shape[-1], x_data_shape[1])
-    for i in range(np.shape(num_time_nuerons)[0]):
+    num_time_nuerons = Funnel(time_flat.shape[-1], x_data_shape[1], r=10)
+    for i in range(1, np.shape(num_time_nuerons)[0]):
         time_flat = Dense(units=num_time_nuerons[i], activation='relu')(time_flat)
-        time_flat = LayerNormalization()(time_flat)
-        time_flat = Dropout(rate=0.1)(time_flat)
+        if (i == 1):
+            for _ in range(num_trans):
+                time_flat = TransformerBlock(embed_dim=time_flat.shape[-1],
+                                             num_heads=4,
+                                             ff_dim=time_flat.shape[-1],
+                                             attn_axes=2)(time_flat)
+        else:
+            time_flat = LayerNormalization()(time_flat)
+            time_flat = Dropout(rate=0.1)(time_flat)
     
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Rejoin
@@ -55,12 +71,6 @@ def MakeDense(config_path,
     final_dense = Concatenate()([time_flat, spat_flat])
 
     flat = Flatten()(final_dense)
-
-    num_flat_neurons = Funnel(flat.shape[-1], int(y_data_shape[2] * y_data_shape[3] * y_data_shape[4] * 10)+1, r=10)
-    for i in range(np.shape(num_flat_neurons)[0]):
-        flat = Dense(units=num_flat_neurons[i], activation='relu')(flat)
-        flat = LayerNormalization()(flat)
-        flat = Dropout(rate=0.1)(flat)
     
     final_dense = Dense(units=y_data_shape[2] * y_data_shape[3] * y_data_shape[4], activation='linear')(flat)
 
@@ -68,25 +78,25 @@ def MakeDense(config_path,
     #----------------------------------------------------------------
     model = keras.Model(input_layer, output_layer)
 
-    # keras.utils.plot_model(model, show_shapes=True, show_layer_activations=True, to_file=os.path.join('SavedModels/Figs', 'DiamondDense.png'))
-    # print(model.summary())
+    keras.utils.plot_model(model, show_shapes=True, show_layer_activations=True, to_file=os.path.join('SavedModels/Figs', 'DiamondDense.png'))
+    print(model.summary())
     return model
 
 #====================================================================
-# x = np.random.random((200, 5, 28, 14, 8))
-# y = np.random.random((200, 1, 28, 14, 1)) * 1.2
+x = np.random.random((200, 5, 28, 14, 8))
+y = np.random.random((200, 1, 28, 14, 1)) * 1.2
 
-# model = MakeDense('config.yml', x.shape, y.shape)
+model = MakeDense('config.yml', x.shape, y.shape)
 
-# model.compile(loss=keras.losses.MeanSquaredError(reduction="sum_over_batch_size", 
-#                                                  name="MSE"),
-#               optimizer=keras.optimizers.Adam(learning_rate=1e-3))
+model.compile(loss=keras.losses.MeanSquaredError(reduction="sum_over_batch_size", 
+                                                 name="MSE"),
+              optimizer=keras.optimizers.Adam(learning_rate=1e-3))
 
-# history = model.fit(x=x,
-#                     y=y,
-#                     batch_size=10,
-#                     epochs=10,
-#                     verbose=1)
+history = model.fit(x=x,
+                    y=y,
+                    batch_size=10,
+                    epochs=10,
+                    verbose=1)
 
 
 
